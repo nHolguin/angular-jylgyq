ca7a3ecb2ff489d9158e724f9be70927d2da9141
nHolguin <nimrodholguin@gmail.com>
2019-08-07 01:03:39 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  animal = ''; 
+  question = 'Te ha gustado la app en Angular?';
+}
